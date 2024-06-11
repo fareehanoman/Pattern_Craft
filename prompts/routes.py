e8b3bb40a9e0de1b6cf19a59872164127b3d0b9a
Mahnoor Khan <manayyykhan123@gmail.com>
2024-06-11 17:22:20 +0500

    Added route for get-user-prompts

--- prompts/routes.py
+++ prompts/routes.py
@@ -6,3 +6,7 @@ prompt_model = Prompt()
 @app.route('/generate-image', methods=['POST'])
 def generate_image():
   return prompt_model.generate_image()
+
+@app.route('/get-user-prompts', methods=['POST'])
+def get_user_prompts():
+  return prompt_model.get_user_prompts()
