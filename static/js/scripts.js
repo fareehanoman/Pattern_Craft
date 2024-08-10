f7a2ee1d8cdb896c308ddc67eae5883d5c6020b3
Fareeha Noman <116712554+fareehanoman@users.noreply.github.com>
2024-08-10 16:57:39 +0500

    added share buttons

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,13 +4,30 @@ $("form[name=signup_form").submit(function(e) {
   var $error = $form.find(".error");
   var data = $form.serialize();
 
+  // Perform front-end validation
+  const emailPattern = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/
+  const email = $form.find("input[name=email]").val();
+  const password = $form.find("input[name=password]").val();
+  
+  if (!emailPattern.test(email)) {
+    $error.text("Invalid email address.").removeClass("error--hidden");
+    e.preventDefault();
+    return;
+  }
+
+  if (password.length < 8) {
+    $error.text("Password must be at least 8 characters long.").removeClass("error--hidden");
+    e.preventDefault();
+    return;
+  }
+
   $.ajax({
     url: "/user/signup",
     type: "POST",
     data: data,
     dataType: "json",
     success: function(resp) {
-      window.location.href = "/home.html";
+      window.location.href = "/login.html";
     },
     error: function(resp) {
       $error.text(resp.responseJSON.error).removeClass("error--hidden");
@@ -41,3 +58,40 @@ $("form[name=login_form").submit(function(e) {
 
   e.preventDefault();
 });
+
+function togglePasswordVisibility() {
+  const passwordField = document.getElementById("password");
+  const passwordToggleIcon = document.getElementById("password-toggle-icon");
+
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    passwordToggleIcon.classList.remove("fa-eye-slash");
+    passwordToggleIcon.classList.add("fa-eye");
+  } else {
+    passwordField.type = "password";
+    passwordToggleIcon.classList.remove("fa-eye");
+    passwordToggleIcon.classList.add("fa-eye-slash");
+  }
+}
+
+const link = encodeURI(window.location.href);
+const msg = encodeURIComponent('Hey, I found this article');
+const title = encodeURIComponent('Article or Post Title Here');
+
+const fb = document.querySelector('.facebook');
+fb.href = `https://www.facebook.com/share.php?u=${link}`;
+
+const twitter = document.querySelector('.twitter');
+twitter.href = `http://twitter.com/share?&url=${link}&text=${msg}&hashtags=javascript,programming`;
+
+const linkedIn = document.querySelector('.linkedin');
+linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${link}`;
+
+const reddit = document.querySelector('.reddit');
+reddit.href = `http://www.reddit.com/submit?url=${link}&title=${title}`;
+
+const whatsapp = document.querySelector('.whatsapp');
+whatsapp.href = `https://api.whatsapp.com/send?text=${msg}: ${link}`;
+
+const telegram = document.querySelector('.telegram');
+telegram.href = `https://telegram.me/share/url?url=${link}&text=${msg}`;
