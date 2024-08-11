2acabf5440fff24b318aa7679efe463de3fd9181
Mahnoor Khan <manayyykhan123@gmail.com>
2024-08-12 00:18:59 +0500

    updated api_info.py

--- prompts/api_info.py
+++ prompts/api_info.py
@@ -1,4 +1,4 @@
 HUGGINGFACE_API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
 # The pattern to follow here is "Bearer TOKEN_API"
 # Please make sure to include Bearer and then enter the token
-headers = {"Authorization": f"Bearer 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"}
+headers = {"Authorization": f"Bearer YOUR_API_TOKEN"}
